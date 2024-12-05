2719aceedc760129da02c9d91698de6d5c4981a0
Petr Pucil <petr.pucil@seznam.cz>
2024-12-05 11:16:40 +0100

    day05_print_queue: solve Part 2

--- day05_print_queue/src/main.rs
+++ day05_print_queue/src/main.rs
@@ -1,27 +1,40 @@
-use std::{collections::{HashMap, HashSet}, io};
+use std::{cmp::Ordering, collections::{HashMap, HashSet}, io};
 
 fn main() {
     let input = read_input_from_stdin();
-    let res = solve_part1(&input);
+    // let res = solve_part1(&input);
+    let res = solve_part2(input);
     println!("{res}");
 }
 
 fn solve_part1(input: &Input) -> u32 {
     input.updates
         .iter()
-        .filter(|u| is_update_correct(u, input))
+        .filter(|u| is_update_correct(u, &input.pages_expected_after))
         .map(|u| middle_page_number(u))
         .sum()
+}
+
+fn solve_part2(input: Input) -> u32 {
+    let mut res = 0;
+    for mut update in input.updates {
+        if is_update_correct(&update, &input.pages_expected_after) {
+            continue;
+        }
+        fix_update(&mut update, &input.pages_expected_after);
+        res += middle_page_number(&update);
+    }
+    res
 }
 
 fn middle_page_number(update: &[u32]) -> u32 {
     update[update.len() / 2]
 }
 
-fn is_update_correct(update: &[u32], input: &Input) -> bool {
+fn is_update_correct(update: &[u32], input_pages_expected_after: &HashMap<u32, HashSet<u32>>) -> bool {
     for (i, page) in update.iter().enumerate() {
         let pages_actually_before = &update[0..i];
-        let Some(pages_expected_after) = input.pages_expected_after.get(&page) else {
+        let Some(pages_expected_after) = input_pages_expected_after.get(&page) else {
             continue;
         };
         for &prev_page in pages_actually_before {
@@ -31,6 +44,22 @@ fn is_update_correct(update: &[u32], input: &Input) -> bool {
         }
     }
     true
+}
+
+fn fix_update(update: &mut [u32], input_pages_expected_after: &HashMap<u32, HashSet<u32>>) {
+    update.sort_by(|page_a, page_b| {
+        if let Some(pages_after_a) = input_pages_expected_after.get(page_a) {
+            if pages_after_a.contains(page_b) {
+                return Ordering::Less;
+            }
+        }
+        if let Some(pages_after_b) = input_pages_expected_after.get(page_b) {
+            if pages_after_b.contains(page_a) {
+                return Ordering::Greater;
+            }
+        }
+        Ordering::Equal
+    });
 }
 
 #[derive(Debug)]
