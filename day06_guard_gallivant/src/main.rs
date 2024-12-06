2739ab5077f4f3f6729992bac92501cb5ae3b95d
Petr Pucil <petr.pucil@seznam.cz>
2024-12-06 09:58:18 +0100

    day06_guard_gallivant: read input as in day04_ceres_search

--- day06_guard_gallivant/src/main.rs
+++ day06_guard_gallivant/src/main.rs
@@ -1,3 +1,108 @@
+use core::str;
+use std::{fmt::Debug, io::{self, BufRead}, ops::{Index, IndexMut}};
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_input_from_stdin();
+    println!("{input:?}");
+}
+
+struct Table {
+    rows: usize,
+    cols: usize,
+    cells: Vec<u8>,
+}
+
+impl Table {
+    fn new(cols: usize, cells: Vec<u8>) -> Self {
+        let rows;
+        if cols != 0 {
+            assert_eq!(cells.len() % cols, 0);
+            rows = cells.len() / cols;
+        } else {
+            assert_eq!(cells.len(), 0);
+            rows = 0;
+        }
+        Self { rows, cols, cells }
+    }
+
+    fn all_positions(&self) -> impl Iterator<Item = Pos> + use<'_> {
+        // https://stackoverflow.com/q/53722749/12940655
+        (0..self.rows).flat_map(|r| (0..self.cols).map(move |c| Pos { r, c }))
+    }
+
+    fn move_from_pos(&self, pos: Pos, dir: (isize, isize), mult: isize) -> Option<Pos> {
+        let r: isize = isize::try_from(pos.r).ok()? + dir.0 * mult;
+        let c: isize = isize::try_from(pos.c).ok()? + dir.1 * mult;
+        let new_pos = Pos { r: r.try_into().ok()?, c: c.try_into().ok()? };
+        if self.contains_pos(new_pos) {
+            Some(new_pos)
+        } else {
+            None
+        }
+    }
+
+    fn contains_pos(&self, pos: Pos) -> bool {
+        pos.r < self.rows && pos.c < self.cols
+    }
+}
+
+impl Debug for Table {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Table ({} rows, {} columns)\n", self.rows, self.cols)?;
+        write!(f, "{{\n")?;
+        for row in self.cells.chunks_exact(self.cols) {
+            write!(f, "  {}\n", str::from_utf8(row).unwrap())?;
+        }
+        write!(f, "}}\n")?;
+        Ok(())
+    }
+}
+
+impl Index<Pos> for Table {
+    type Output = u8;
+
+    fn index(&self, pos: Pos) -> &Self::Output {
+        assert!(self.contains_pos(pos));
+        &self.cells[pos.r * self.cols + pos.c]
+    }
+}
+
+impl IndexMut<Pos> for Table {
+    fn index_mut(&mut self, pos: Pos) -> &mut Self::Output {
+        assert!(pos.r < self.rows);
+        assert!(pos.c < self.cols);
+        &mut self.cells[pos.r * self.cols + pos.c]
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct Pos {
+    r: usize,
+    c: usize,
+}
+
+fn read_input_from_stdin() -> Table {
+    let mut cells = vec![];
+    let mut stdin = io::stdin().lock();
+    let mut line = vec![];
+    stdin.read_until(b'\n', &mut line).unwrap();
+    if line.last().map_or(false, |&x| x == b'\n') {
+        line.pop();
+    }
+    let cols = line.len();
+    cells.append(&mut line);
+
+    loop {
+        stdin.read_until(b'\n', &mut line).unwrap();
+        if line.is_empty() {
+            break;
+        }
+        if line.last().map_or(false, |&x| x == b'\n') {
+            line.pop();
+        }
+        assert_eq!(line.len(), cols);
+        cells.append(&mut line);
+    }
+
+    Table::new(cols, cells)
 }
