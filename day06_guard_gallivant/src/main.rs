faddb0f158cff85fa906ea0fac6611243159caae
Petr Pucil <petr.pucil@seznam.cz>
2024-12-08 00:54:51 +0100

    day06_guard_gallivant: solve Part 2

--- day06_guard_gallivant/src/main.rs
+++ day06_guard_gallivant/src/main.rs
@@ -1,9 +1,10 @@
 use core::str;
-use std::{fmt::Debug, io::{self, BufRead}, ops::{Index, IndexMut}};
+use std::{collections::HashSet, fmt::Debug, io::{self, BufRead}, ops::{Index, IndexMut}};
 
 fn main() {
     let input = read_input_from_stdin();
-    let res = solve_part1(input);
+    // let res = solve_part1(input);
+    let res = solve_part2(input);
     println!("{res}");
 }
 
@@ -30,10 +31,63 @@ fn solve_part1(mut table: Table) -> usize {
     num_distinct_positions
 }
 
+fn solve_part2(mut table: Table) -> usize {
+    let mut guard_pos = table.all_positions()
+        .find(|&pos| table[pos] == b'^')
+        .unwrap();
+    let mut guard_dir = (-1, 0);
+    let mut num_loops = 0;
+    loop {
+        table[guard_pos] = b'X';
+        let Some(next_pos) = table.move_from_pos(guard_pos, guard_dir, 1) else {
+            break;
+        };
+        if table[next_pos] == b'#' {
+            guard_dir = dir_right_hand(guard_dir);
+            continue;
+        }
+        let orig_cell = table[next_pos];
+        if orig_cell == b'.' {
+            table[next_pos] = b'#';
+            if is_stuck_in_loop(&table, guard_pos, guard_dir) {
+                num_loops += 1;
+                table[next_pos] = b'@';
+            } else {
+                table[next_pos] = orig_cell;
+            }
+        }
+        guard_pos = next_pos;
+    }
+    num_loops
+}
+
+fn is_stuck_in_loop(table: &Table, mut guard_pos: Pos, mut guard_dir: (isize, isize)) -> bool {
+    let mut prev_states: HashSet<(Pos, (isize, isize))> = HashSet::new();
+    for _ in 0..10_000 {
+        {
+            let state = (guard_pos, guard_dir);
+            if prev_states.contains(&state) {
+                return true;
+            }
+            prev_states.insert(state);
+        }
+        let Some(next_pos) = table.move_from_pos(guard_pos, guard_dir, 1) else {
+            return false;
+        };
+        if table[next_pos] == b'#' {
+            guard_dir = dir_right_hand(guard_dir);
+            continue;
+        }
+        guard_pos = next_pos;
+    }
+    unreachable!()
+}
+
 fn dir_right_hand(dir: (isize, isize)) -> (isize, isize) {
     (dir.1, -dir.0)
 }
 
+#[derive(Clone)]
 struct Table {
     rows: usize,
     cols: usize,
@@ -103,7 +157,7 @@ impl IndexMut<Pos> for Table {
     }
 }
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct Pos {
     r: usize,
     c: usize,
@@ -143,5 +197,33 @@ mod tests {
     fn right_hand() {
         assert_eq!(dir_right_hand((0, 1)), (1, 0));
         assert_eq!(dir_right_hand((1, 0)), (0, -1));
+    }
+
+    #[test]
+    fn stuck_in_loop() {
+        let table = Table::new(4, vec![
+            b'.', b'#', b'.', b'.',
+            b'.', b'.', b'.', b'#',
+            b'#', b'^', b'.', b'.',
+            b'.', b'.', b'#', b'.',
+        ]);
+        assert!(is_stuck_in_loop(&table, table.all_positions().find(|&pos| table[pos] == b'^').unwrap(), (-1, 0)));
+
+        let table = Table::new(4, vec![
+            b'.', b'#', b'.', b'.',
+            b'.', b'.', b'.', b'#',
+            b'.', b'^', b'.', b'.',
+            b'.', b'.', b'#', b'.',
+        ]);
+        assert!(!is_stuck_in_loop(&table, table.all_positions().find(|&pos| table[pos] == b'^').unwrap(), (-1, 0)));
+
+        let table = Table::new(6, vec![
+            b'.', b'#', b'.', b'#', b'.', b'.',
+            b'.', b'.', b'.', b'.', b'.', b'#',
+            b'#', b'.', b'v', b'.', b'.', b'.',
+            b'.', b'.', b'#', b'.', b'.', b'.',
+            b'.', b'.', b'.', b'.', b'#', b'.',
+        ]);
+        assert!(is_stuck_in_loop(&table, table.all_positions().find(|&pos| table[pos] == b'v').unwrap(), (1, 0)));
     }
 }
